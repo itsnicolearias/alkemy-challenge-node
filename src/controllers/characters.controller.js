ae8f9af43ef9d082f1f1ae3d29b377b10caf584b
itsnicolearias <nicolmccann@gmail.com>
2022-05-24 19:56:56 -0300

    feat: add characters crud

--- src/controllers/characters.controller.js
+++ src/controllers/characters.controller.js
@@ -1,45 +1,80 @@
+import { Characters } from '../models/characters.model.js'
 
 export const getAllCharacters = async(req, res) => {
-
     try {
-        
+        const characters = await Characters.findAll({
+            attributes: ["image", "name"],
+        });
+       
+        res.json({characters: characters})
     } catch (error) {
-        
+        res.status(500).json({
+            message: error.message,
+          });
     }
 }
 
 export const getCharacterById = async(req, res) => {
-
+    const { id } = req.params
     try {
-        
+        const character = await Characters.findOne({
+            where: {id}
+        })
+        res.json(character)
     } catch (error) {
         
     }
 }
 
 export const createCharacter = async(req, res) => {
+    const { image, name, age, weight, history, asociated_movies } = req.body
 
     try {
-        
+        const newCharacter = await Characters.create({
+            image,
+            name,
+            age,
+            weight,
+            history,
+            asociated_movies
+
+        })
+        res.json(newCharacter)
     } catch (error) {
-        
+        res.status(500).json({
+            message: error.message,
+          });
     }
 }
 
 export const updateCharacter = async(req, res) => {
-
+    const { id } = req.params;
+    
     try {
-        
+        const character = await Characters.findOne({
+            where: {id}
+        })
+        character.set(req.body)
+        await character.save()
+        res.json(character)
     } catch (error) {
-        
+        res.status(500).json({
+            message: error.message,
+          });
     }
 }
 
 export const deleteCharacter = async(req, res) => {
+    const { id } = req.params;
 
     try {
-        
+        await Characters.destroy({
+            where: {id}
+        })
+        res.sendStatus(204)
     } catch (error) {
-        
+        res.status(500).json({
+            message: error.message,
+          });
     }
 }
