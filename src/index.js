7dc5f48f8e3cd3751a0728b0a867b8cf67c7c4f7
itsnicolearias <nicolmccann@gmail.com>
2022-05-30 19:10:33 -0300

    fix errors in index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import app from './app.js';
 import { enviromentConfig } from './config/enviromentConfig.js'
 import { sequelize } from './config/database.js'
 
-
+//import models
+import './models/user.model.js'
+import './models/characters.model.js'
+import './models/movies.model.js'
+import './models/genre.model.js'
+import './models/references.js'
 
 async function main(){
     try {
-        await sequelize.sync({ force: false})
+        await sequelize.sync({ alter: true })
         
         app.listen(enviromentConfig.app.port)
         console.log('Server running')
